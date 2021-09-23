afada97d59d26bc460c44eb6d289782c41f0e8e6
Magnum <magnum.alvarado@gmail.com>
2021-09-23 11:43:13 -0300

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+import Home from './components/home/home';
+import GoogleLogin from 'ract-google-login';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Home />
   );
+}
+
+export default App;
+
+function App() {
+	const [name, setName] = useState();
+	const [email, setEmail] = useState();
+	const [profilePic, setProfilePic] = useState();
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+	const responseGoogle = (response) => {
+		console.log(response);
+		const {
+			profileObj: { name, email, imageUrl },
+		} = response;
+		setName(name);
+		setEmail(email);
+		setProfilePic(imageUrl);
+		setIsLoggedIn(true);
+	};
+	return (
+		<div className="container">
+			<GoogleLogin
+				clientId="885442628950-1tgtma80butija0bsp6a530p0jp0d9od.apps.googleusercontent.com"
+				buttonText="Continuar com o Google"
+				onSuccess={responseGoogle}
+				onFailure={responseGoogle}
+			/>
+			{isLoggedIn ? (
+				<div style={{ textAlign: "center" }}>
+					<h1>User Information</h1>
+					<img className="profile" src={profilePic} alt="Profile" />
+					<p>Name: {name}</p>
+					<p>Email: {email}</p>
+				</div>
+			) : (
+				""
+			)}
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/home/home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './home.css';
+
+function Home() {
+    return (
+      <div className="HomePage">
+          <header className="HomeHeader">
+    <h1>Feature Paypal</h1>
+    </header>
+      </div>
+   
+    );
+  }
+
+  export default Home;
